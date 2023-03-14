b74fbb0ae9837b00339e6bbf66737eb5d3a08dba
susu3131 <anny81327@gmail.com>
2023-03-15 00:01:32 +0800

    後台表格

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="bg-puzzle bg-contain">
     <!--  header -->
-    <div class="flex sm:py-3 items-center justify-center p-5 sm:pt-10 sm:px-10">
+    <div class="flex sm:py-3 items-center justify-center p-5 sm:pt-10 sm:px-10 lg:pb-0">
+      <div class="lg:flex-1"></div>
       <!-- tittle -->
-      <div class="flex items-center justify-center grow px-2  sm:mb-0">
+      <div class="flex items-center justify-center grow px-2 sm:mb-0 lg:shrink-0">
         <img class="lg:w-[100px]" src=".././assets/image/logo.png" alt="logo" width="80" />
         <h1 class="text-2xl lg:text-4xl font-extrabold text-primary">
           拼圖趣 Puzzle
@@ -11,34 +12,35 @@
         </h1>
       </div>
       <!-- 登出按鈕 -->
-      <div class="flex py-2 justify-center ml-3 sm:ml-0  lg:mt-5">
-        <div class="md:flex items-center justify-center mr-4  hidden ">
+      <div class="flex py-2 justify-center ml-3 sm:ml-0 lg:mt-5 lg:flex-1">
+        <div class="md:flex items-center justify-center mr-4 hidden">
           <i class="fa-solid fa-circle-user"></i>
           <p class="ml-2">管理員 admin</p>
         </div>
-        <button class="flex items-center justify-center border-dark border-[1px] hover:bg-primary hover:text-white px-3  sm:px-6 py-1 h-1/4 rounded-md">
-          <p class="mr-1 hidden sm:block text-base">登出</p>
+        <button class="flex items-center justify-center border-dark border-[1px] hover:bg-primary hover:text-white px-3 sm:px-6 py-1 h-1/4 rounded-md">
+          <p class="mr-1 hidden md:block text-base">登出</p>
           <i class="fa-solid fa-right-from-bracket ml-1"></i>
         </button>
       </div>
     </div>
 
     <!-- content -->
-    <div class="flex text-center  sm:p-0">
-      <router-link to="/order" class="sm:py-3 sm:px-10 py-2 px-6 hover:bg-secondary hover:text-white hover:font-bold border-2 border-secondary border-b-0">訂單管理</router-link>
-      <router-link to="/puzzle" class="sm:py-3 sm:px-10 py-2 px-6 hover:bg-secondary hover:text-white hover:font-bold border-2 border-l-0 border-secondary border-b-0">商品管理</router-link>
+    <div class="flex text-center sm:p-0 font-bold">
+      <router-link to="/order" class=" sm:py-3 sm:px-10 py-2 px-6 hover:bg-secondary hover:text-white hover:font-black border-2 border-secondary border-b-0">訂單管理</router-link>
+      <router-link to="/puzzle" class="sm:py-3 sm:px-10 py-2 px-6 hover:bg-secondary hover:text-white hover:font-black  border-2 border-l-0 border-secondary border-b-0">商品管理</router-link>
     </div>
+
     <!-- routeview -->
     <div class="bg-white opacity-80 border-y-2 h-screen border-secondary">
+      <!-- home  -->
+      <div class="bg-white opacity-80">
+        <div class="text-blue-600 text-xl">
+          <RouterLink to="/index">回到首頁</RouterLink>
+          <RouterLink to="/g" class="ml-5">404 測試</RouterLink>
+        </div>
+      </div>
       <RouterView />
     </div>
-    <!-- home  -->
-    <!-- <div class="bg-white opacity-80">
-      <div class="text-blue-600 text-xl">
-        <RouterLink to="/index">回到首頁</RouterLink>
-        <RouterLink to="/g" class="ml-5">404 測試</RouterLink>
-      </div>
-    </div> -->
   </div>
 </template>
 
--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,15 +1,98 @@
 <template>
-  <div>
-    訂製拼圖
+  <div class="flex flex-col border">
+  <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+      <div class="overflow-hidden">
+        <table class="min-w-full text-left text-sm font-light">
+          <thead
+            class="border-b bg-white font-medium dark:border-neutral-500 dark:bg-neutral-600">
+            <tr>
+              <th scope="col" class="px-6 py-4">#</th>
+              <th scope="col" class="px-6 py-4">First</th>
+              <th scope="col" class="px-6 py-4">Last</th>
+              <th scope="col" class="px-6 py-4">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              class="border-b bg-neutral-100 dark:border-neutral-500 dark:bg-neutral-700">
+              <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
+              <td class="whitespace-nowrap px-6 py-4">Mark</td>
+              <td class="whitespace-nowrap px-6 py-4">Otto</td>
+              <td class="whitespace-nowrap px-6 py-4">@mdo</td>
+            </tr>
+            <tr
+              class="border-b bg-white dark:border-neutral-500 dark:bg-neutral-600">
+              <td class="whitespace-nowrap px-6 py-4 font-medium">2</td>
+              <td class="whitespace-nowrap px-6 py-4">Jacob</td>
+              <td class="whitespace-nowrap px-6 py-4">Thornton</td>
+              <td class="whitespace-nowrap px-6 py-4">@fat</td>
+            </tr>
+            <tr
+              class="border-b bg-neutral-100 dark:border-neutral-500 dark:bg-neutral-700">
+              <td class="whitespace-nowrap px-6 py-4 font-medium">3</td>
+              <td
+                colspan="2"
+                class="whitespace-nowrap px-6 py-4 text-center">
+                Larry the Bird
+              </td>
+              <td class="whitespace-nowrap px-6 py-4">@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  <div class="p-3">
+    <h2 class="text-left mb-4">訂單管理</h2>
+
+    <table class="border-2 w-full  text-center md:text-left  border-separate  p-1 ">
+      <thead class=" text-center md:text-left">
+        <tr  >
+          <th >商品圖片</th>
+          <th class="hidden">商品名稱</th>
+          <th>數量</th>
+          <th>金額</th>
+          <th>
+            <p>訂購資訊</p>
+          </th>
+          <th class="hidden">訂單狀態</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><img src=".././assets/image/product/1.jpg" class="w-24  mx-auto md:m-0" alt="img" /></td>
+          <td class="hidden">[ 1000片 ] - 春眠 (もの久保 )</td>
+          <td >
+            <div class="border px-3 py-1 md:px-4 md:py-2 inline">1</div>
+          </td>
+          <td>$2000</td>
+          <td>
+            <div class="hidden">
+              <p>名字: 王曉蕾</p>
+              <p>電話:0912-345-429</p>
+              <p>信箱 :wang@gmail.com</p>
+              <p>地址: 252 新北市三芝區觀海街10號</p>
+            </div>
+            <button class="border px-2 py-1 hover:bg-secondary hover:text-white">查看訂單</button>
+          </td>
+          <td class="flex items-center hidden">
+            <div class="border">
+              <i class="fa-solid fa-xmark px-3 py-2 text-base"></i>
+            </div>
+            <p class="border p-2">訂單完成</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
