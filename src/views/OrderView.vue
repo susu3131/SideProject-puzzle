ac6de7d7bd54c76d17d6b67fb36428fbfcd7ccb9
您的姓名 <anny81327@gmail.com>
2023-03-21 16:51:18 +0800

    後台表格Rwd切換

--- src/views/OrderView.vue
+++ src/views/OrderView.vue
@@ -1,63 +1,68 @@
 <template>
-    <div class="p-3 lg:px-20  ">
-      <div class="flex items-center justify-between">
-        <h2 class="text-left mb-4">訂單管理</h2>
-        <!-- home  -->
-        <div class="bg-white opacity-80">
-          <div class="text-blue-600 text-xl">
-            <RouterLink to="/index">回到首頁</RouterLink>
-            <RouterLink to="/g" class="ml-5">404 測試</RouterLink>
-          </div>
+  <div class="p-3 lg:px-20">
+    <div class="flex items-center justify-between">
+      <h2 class="text-left mb-4">訂單管理</h2>
+      <!-- home  -->
+      <div class="bg-white opacity-80">
+        <div class="text-blue-600 text-xl">
+          <RouterLink to="/index">回到首頁</RouterLink>
+          <RouterLink to="/g" class="ml-5">404 測試</RouterLink>
         </div>
-      </div>
-
-      <div class="overflow-x-auto">
-        <table class="table w-full border border-dark">
-          <!-- head -->
-          <thead class="p-2">
-            <tr>
-              <th>商品圖片</th>
-              <th class="hidden md:block">商品名稱</th>
-              <th>數量</th>
-              <th>金額</th>
-              <th>訂購資訊</th>
-              <th class="hidden md:block">訂單狀態</th>
-            </tr>
-          </thead>
-          <tbody>
-            <!-- row 1 -->
-            <tr>
-              <td><img src=".././assets/image/product/1.jpg" class="w-24 mx-auto md:m-0" alt="img" /></td>
-              <td class="hidden md:block">[ 1000片 ] - 春眠 (もの久保 )</td>
-              <td>
-                <div class="border px-3 py-1 md:px-4 md:py-2 inline">1</div>
-              </td>
-              <td>$2000</td>
-              <td>
-                <div class="hidden md:block">
-                  <p>名字: 王曉蕾</p>
-                  <p>電話:0912-345-429</p>
-                  <p>信箱 :wang@gmail.com</p>
-                  <p>地址: 252 新北市三芝區觀海街10號</p>
-                </div>
-                <button class="border px-2 py-1 hover:bg-secondary hover:text-white">查看訂單</button>
-              </td>
-              <td class="items-center hidden md:block">
-                <div class="border">
-                  <i class="fa-solid fa-xmark px-3 py-2 text-base"></i>
-                </div>
-                <p class="border p-2">訂單完成</p>
-              </td>
-            </tr>
-            <!-- row 2 -->
-            <tr></tr>
-            <!-- row 3 -->
-            <tr></tr>
-          </tbody>
-        </table>
       </div>
     </div>
 
+    <div class="overflow-x-auto">
+      <table class="table w-full border table-auto text-black border-black">
+        <!-- head -->
+        <thead class="p-2 hidden md:table-header-group">
+          <tr>
+            <th>商品圖片</th>
+            <th class="hidden md:block">商品名稱</th>
+            <th>數量</th>
+            <th>金額</th>
+            <th>訂購資訊</th>
+            <th class="hidden md:block">訂單狀態</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- row 1 -->
+          <tr class="flex flex-col border md:table-row">
+            <td class="hidden ">
+              <img src=".././assets/image/product/1.jpg" class="md:w-24 mx-auto md:m-0 w-32" alt="img" />
+            </td>
+            <td class="border-b border-gray-300">
+              <p class="md:hidden font-bold mb-1">商品名稱</p>
+              <p>[ 1000片 ] - 春眠 (もの久保 )</p>
+            </td>
+            <td class="border-b border-gray-300">
+              <p class="md:hidden font-bold mb-1">數量</p>
+              <div class="border px-3 py-1 md:px-4 md:py-2 inline">1</div>
+            </td>
+            <td class="border-b border-gray-300">
+              <p class="md:hidden font-bold mb-1">金額</p>
+              <p>$2000</p>
+            </td>
+            <td class="border-b border-gray-300">
+              <p class="md:hidden font-bold mb-1">訂購資訊</p>
+              <div class="=">
+                <p>名字: 王曉蕾</p>
+                <p>電話:0912-345-429</p>
+                <p>信箱 :wang@gmail.com</p>
+                <p>地址: 252 新北市三芝區觀海街10號</p>
+              </div>
+            </td>
+            <td class="items-center flex">
+              <div class="border">
+                <i class="fa-solid fa-xmark px-3 py-2 text-base"></i>
+              </div>
+              <p class="border p-2">訂單完成</p>
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       //客製寫在這
       colors: {
         'primary': '#F56257', //橘
-        'secondary': '#95DACD', //綠
+        'secondary': '#82D0C1', //綠
         'pink': '#FF8888', //粉
         'dark': '#333333', //暗黑
         'light': '#FFAA4D' //黃
