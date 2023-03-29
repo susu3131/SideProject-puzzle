94cc1468c68b692c6956b24fcdf81c9b0ac31b64
您的姓名 <anny81327@gmail.com>
2023-03-29 10:01:22 +0800

    櫻花特效

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 // snowStorm.autoStart = true
-snowStorm.snowColor = '#FF8888';   // blue-ish snow!?
-snowStorm.flakesMax = 800;
-snowStorm.animationInterval = 30;
-snowStorm.flakesMaxActive = 150;    // show more snow on screen at once
-snowStorm.snowStick = false;
-snowStorm.usePositionFixed = true;
+// snowStorm.snowColor = '#FF8888';   // blue-ish snow!?
+// snowStorm.flakesMax = 100;
+// snowStorm.animationInterval = 30;
+// snowStorm.flakesMaxActive = 150;    // show more snow on screen at once
+// snowStorm.snowStick = false;
+// snowStorm.usePositionFixed = true;
+var sakura = new Sakura('body', {
+    fallSpeed:0.5,
+    // minSize: 10,
+    // maxSize: 20,
+    // delay:100,
+});
 </script>
 
 <template>
