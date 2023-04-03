e43d29cbea21653fbfe35914a7b1c4205fb566ed
susu3131 <anny81327@gmail.com>
2023-04-03 16:48:39 +0800

    nav ok & 加入判斷畫面大小顯示搜尋列

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="flex flex-col justify-between">
       <!-- header -->
       <header class="container pt-3">
-        <div class="sm:grid sm:grid-cols-12 items-center">
+        <div class="xl:grid xl:grid-cols-12 items-center">
           <div class="hidden sm:block col-span-2"></div>
 
           <!-- phone nav -->
@@ -13,18 +13,18 @@
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </button>
             <div class="collapse-content z-50 -ml-4">
-              <ul class="menu bg-white border-t-2 border-b-2 border-primary w-screen  to-base-100">
-                <li class="border-b ">
-                  <div class="flex justify-center items-center  text-white bg-primary active:bg-opacity-50">
+              <ul class="menu bg-white border-t-2 border-b-2 border-primary w-screen to-base-100">
+                <li class="border-b">
+                  <div class="flex justify-center items-center text-white bg-primary active:bg-opacity-50">
                     <label for="login-modal" class="flex items-center justify-center w-full">
                       <i class="fa-solid fa-circle-user mr-2"></i>
                       <p>Login</p>
                     </label>
                   </div>
                 </li>
-                <li class="border-b" >
+                <li class="border-b">
                   <div class="flex justify-center">
-                    <i class="fa-solid fa-magnifying-glass  p-2"></i>
+                    <i class="fa-solid fa-magnifying-glass p-2"></i>
                     <label for="search" class="mr-2 w-full">
                       <input type="search" name="search" id="" placeholder="搜尋喜愛的拼圖 ..." value="" class="p-2 rounded-md border text-sm text-gray-400 focus:border-primary focus:border-2 focus-visible:outline-none w-full" />
                     </label>
@@ -68,7 +68,7 @@
           </div>
 
           <!-- tittle & pc nav -->
-          <div class="sm:col-span-7">
+          <div class="lg:col-span-7">
             <!-- tittle -->
             <div class="flex items-center sm:justify-center justify-center">
               <img src=".././assets/image/logo.png" alt="logo" class="w-20 md:w-24" />
@@ -77,36 +77,36 @@
             <!-- pc nav -->
             <nav class="font-bold text-center text-lg sm:flex justify-center mt-1 hidden">
               <RouterLink to="/index">
-                <div class="border-r border-black flex items-center pr-8 pl-8 hover:text-primary hover-button">
-                  <i class="fa-solid fa-house mr-4"></i>
+                <div class="border-r border-black lg:flex items-center pr-8 pl-8 hover:text-primary hover-button">
+                  <i class="fa-solid fa-house lg:mr-4"></i>
                   <p>首頁</p>
                 </div>
               </RouterLink>
               <RouterLink to="/products">
-                <div class="border-r border-black flex items-center pr-8 pl-8 hover-button">
-                  <i class="fa-solid fa-puzzle-piece mr-4"></i>
+                <div class="border-r border-black lg:flex items-center pr-8 pl-8 hover-button">
+                  <i class="fa-solid fa-puzzle-piece lg:mr-4"></i>
                   <p>精選商品</p>
                 </div>
               </RouterLink>
               <RouterLink to="/customize">
-                <div class="border-r border-black flex items-center pr-8 pl-8 hover-button">
-                  <i class="fa-solid fa-pen-to-square mr-4"></i>
+                <div class="border-r border-black lg:flex items-center pr-8 pl-8 hover-button">
+                  <i class="fa-solid fa-pen-to-square lg:mr-4"></i>
                   <p>訂製拼圖</p>
                 </div>
               </RouterLink>
               <RouterLink to="/customize">
-                <div class="flex items-center pr-8 pl-8 hover-button">
-                  <i class="fa-solid fa-pen-to-square mr-4"></i>
+                <div class="lg:flex items-center pr-8 pl-8 hover-button">
+                  <i class="fa-solid fa-pen-to-square lg:mr-4"></i>
                   <p>我的收藏</p>
                 </div>
               </RouterLink>
             </nav>
           </div>
 
-          <div class="sm:flex items-center col-span-3 justify-end text-lg pt-6 hidden">
+          <div class="sm:flex items-center md:col-span-3 xl:justify-end justify-center text-lg py-6 lg:pb-0 hidden">
             <i class="fa-solid fa-magnifying-glass mr-3 p-2 hover:bg-primary hover:rounded-full hover:text-white" @click="showSearch = true"></i>
-            <label for="search" class="mr-2" v-if="showSearch">
-              <input type="search" name="search" id="" value="搜尋喜愛的拼圖 ..." class="p-2 rounded-md border text-sm text-gray-400 focus:border-primary focus:border-2 focus-visible:outline-none" />
+            <label for="search" class="mr-2 w-full md:w-1/2" v-if="showSearch">
+              <input type="search" name="search" id="" value="搜尋拼圖 ..." class="p-2 w-full rounded-md border text-sm text-gray-400 focus:border-primary focus:border-2 focus-visible:outline-none" />
             </label>
 
             <label for="cart-modal">
@@ -176,9 +176,21 @@ export default {
       showNav: false
     }
   },
+  methods: {
+    closeSearch() {
+      if (window.innerWidth <= 1024) {
+        this.showSearch = true
+      } else {
+        this.showSearch = false
+      }
+    }
+  },
   components: {
     ModalItem,
     CartItem
+  },
+  mounted() {
+    this.closeSearch();
   }
 }
 </script>
@@ -201,6 +213,16 @@ export default {
   }
 }
 
+@media screen and (min-width: 640px) and (max-width: 1024px) {
+  .hover-button {
+    position: relative;
+    &::before {
+      content: '';
+      @apply bg-primary absolute -bottom-2 h-[2px] left-5 mx-auto;
+      transform: scale(0);
+    }
+  }
+}
 // .menu :where(li:not(.menu-title):not(:empty)) > :where(*:not(ul):active) {
 //   --tw-bg-opacity: 0;
 //   background-color: white; /* 更改此處的顏色 */
