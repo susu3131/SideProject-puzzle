786a52005997c465cf01039e68de06597f63a606
su <anny81327@gmail.com>
2023-07-03 15:12:22 +0800

    商品列表 nav rwd

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="about py-10 container">
+  <div class="about md:py-10 container">
     <!-- banner -->
-    <img src="../assets/image/product-banner.jpg" alt="" />
+    <img src="../assets/image/product-banner.jpg" alt="" class=" h-24 w-full  object-cover object-center sm:h-auto " />
     <!-- nav -->
-    <div class="bg-pink text-white flex justify-between items-center">
-      <button class="bg-white py-2 my-3 px-4 text-black flex items-center rounded-lg hover:bg-secondary hover:outline-white hover:outline ml-4">
+    <div class="bg-pink  text-white lg:flex justify-between items-center  ">
+      <button class=" border bg-white py-2  lg:w-[230px] lg:justify-center  px-4 w-full  text-black flex  items-center lg:rounded-lg hover:bg-secondary lg:hover:outline-white lg:hover:outline lg:ml-4">
         <i class="fa-solid fa-gift mr-2"></i>
-        <p>領取本日優惠卷</p>
+        <p class="">領取本日優惠卷</p>
       </button>
-
-      <RouterLink to="/allpuzzle" class="mr-5 text-xl hover:bg-white py-4 px-10 hover:text-primary">全部拼圖</RouterLink>
-      <RouterLink to="/newpuzzle" class="mr-5 text-xl hover:bg-white py-4 px-10 hover:text-primary">新品上市</RouterLink>
-      <RouterLink to="/salepuzzle" class="mr-5 text-xl hover:bg-white py-4 px-10 hover:text-primary">促銷拼圖</RouterLink>
-      <RouterLink to="/puzzlepiece" class="mr-5 text-xl hover:bg-white py-4 px-10 hover:text-primary">拼圖片數</RouterLink>
-      <RouterLink to="/onlypuzzle" class="mr-5 text-xl hover:bg-white py-4 px-10 hover:text-primary">獨家/ 限量拼圖</RouterLink>
+      <div class=" flex  justify-around md:w-full  md:mr-5   text-center lg:pl-5 ">
+        <RouterLink to="/allpuzzle" class=" border-r  sm:border-0  lg:text-xl hover:bg-white lg:hover:scale-105 xl:hover:scale-125  duration-300 px-5 py-4  md:py-4 md:px-6 lg:px-10 lg:py-6 hover:text-primary">全部拼圖</RouterLink>
+        <RouterLink to="/newpuzzle" class=" border-r sm:border-0  lg:text-xl hover:bg-white  lg:hover:scale-105 xl:hover:scale-125  duration-300  px-5 py-4 md:py-4 md:px-6 lg:px-10 lg:py-6 hover:text-primary">新品上市</RouterLink>
+        <RouterLink to="/salepuzzle" class=" border-r sm:border-0 lg:text-xl hover:bg-white  lg:hover:scale-105 xl:hover:scale-125  duration-300  px-5 py-4 md:py-4 md:px-6 lg:px-10 lg:py-6 hover:text-primary">特賣拼圖</RouterLink>
+        <RouterLink to="/puzzlepiece" class=" border-r sm:border-0  lg:text-xl hover:bg-white  lg:hover:scale-105 xl:hover:scale-125  duration-300  px-5 py-4 md:py-4 md:px-6 lg:px-10 lg:py-6 hover:text-primary">拼圖片數</RouterLink>
+        <RouterLink to="/onlypuzzle" class="   lg:text-xl hover:bg-white xl:hover:scale-125  lg:hover:scale-105  duration-300 md:py-4  px-5 py-4 md:px-6 lg:px-10 lg:py-6 hover:text-primary  ">限量拼圖</RouterLink>
+      </div>
     </div>
-
 
     <RouterView></RouterView>
   </div>
