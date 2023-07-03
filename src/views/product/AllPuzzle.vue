9d04128345ae9f4a62084ba7f0c396cb94bac041
su <anny81327@gmail.com>
2023-07-03 14:28:12 +0800

    前台畫面html標籤

--- src/views/product/AllPuzzle.vue
+++ src/views/product/AllPuzzle.vue
@@ -1,12 +1,70 @@
 <template>
-  <div>
-    <div class="flex">
-      <button class="flex">
-        
+  <div class="py-10">
+    <div class="flex  justify-between px-2">
+      <button class="flex items-center  text-lg hover:font-semibold hover:scale-110 ">
+        <i class="fa-solid fa-circle-chevron-left mr-4"></i>
         <p>上一頁</p>
       </button>
-      <div></div>
+
+      <button class="flex items-center text-lg  hover:font-semibold hover:scale-110 ">
+        <p>下一頁</p>
+        <i class="fa-solid fa-circle-chevron-right ml-4"></i>
+      </button>
     </div>
+
+    <!-- product -->
+    <ul class="grid grid-cols-4 gap-6 mt-5">
+      <li class=" text-center">
+        <img src="../../assets/image/product/product(1).jpg" alt="" class=" rounded-md">
+        <h3 class="mt-2   text-lg">[ 1200片 ] - 相遇 (もの久保 )</h3>
+        <p class="text-xl text-pink  font-medium">售價 : NT $ 1400 </p>
+        <button class="px-9 py-1 mt-2 text-white rounded-md bg-primary  duration-300     hover:scale-110">加入購物車</button>
+      </li>
+      <li class=" text-center">
+        <img src="../../assets/image/product/product(1).jpg" alt="" class=" rounded-md">
+        <h3 class="mt-2   text-lg">[ 1200片 ] - 相遇 (もの久保 )</h3>
+        <p class="text-xl text-pink  ">售價 : NT $ 1400 </p>
+        <button class="px-9 py-1 mt-2 text-white rounded-md bg-primary ">加入購物車</button>
+      </li>
+      <li class=" text-center">
+        <img src="../../assets/image/product/product(1).jpg" alt="" class=" rounded-md">
+        <h3 class="mt-2   text-lg">[ 1200片 ] - 相遇 (もの久保 )</h3>
+        <p class="text-xl text-pink  ">售價 : NT $ 1400 </p>
+        <button class="px-9 py-1 mt-2 text-white rounded-md bg-primary ">加入購物車</button>
+      </li>
+      <li class=" text-center">
+        <img src="../../assets/image/product/product(1).jpg" alt="" class=" rounded-md">
+        <h3 class="mt-2   text-lg">[ 1200片 ] - 相遇 (もの久保 )</h3>
+        <p class="text-xl text-pink  ">售價 : NT $ 1400 </p>
+        <button class="px-9 py-1 mt-2 text-white rounded-md bg-primary ">加入購物車</button>
+      </li>
+      <li class=" text-center">
+        <img src="../../assets/image/product/product(1).jpg" alt="" class=" rounded-md">
+        <h3 class="mt-2   text-lg">[ 1200片 ] - 相遇 (もの久保 )</h3>
+        <p class="text-xl text-pink  ">售價 : NT $ 1400 </p>
+        <button class="px-9 py-1 mt-2 text-white rounded-md bg-primary ">加入購物車</button>
+      </li>
+      <li class=" text-center">
+        <img src="../../assets/image/product/product(1).jpg" alt="" class=" rounded-md">
+        <h3 class="mt-2   text-lg">[ 1200片 ] - 相遇 (もの久保 )</h3>
+        <p class="text-xl text-pink  ">售價 : NT $ 1400 </p>
+        <button class="px-9 py-1 mt-2 text-white rounded-md bg-primary ">加入購物車</button>
+      </li>
+      <li class=" text-center">
+        <img src="../../assets/image/product/product(1).jpg" alt="" class=" rounded-md">
+        <h3 class="mt-2   text-lg">[ 1200片 ] - 相遇 (もの久保 )</h3>
+        <p class="text-xl text-pink  ">售價 : NT $ 1400 </p>
+        <button class="px-9 py-1 mt-2 text-white rounded-md bg-primary ">加入購物車</button>
+      </li>
+      <li class=" text-center">
+        <img src="../../assets/image/product/product(1).jpg" alt="" class=" rounded-md">
+        <h3 class="mt-2   text-lg">[ 1200片 ] - 相遇 (もの久保 )</h3>
+        <p class="text-xl text-pink  ">售價 : NT $ 1400 </p>
+        <button class="px-9 py-1 mt-2 text-white rounded-md bg-primary ">加入購物車</button>
+      </li>
+
+    </ul>
+
   </div>
 </template>
 
